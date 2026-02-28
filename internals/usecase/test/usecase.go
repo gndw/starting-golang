7b182c77ea130e38d8fb33492e715e3ba45200ab
Andrian Gandawida <andrian.gandawida@gmail.com>
2026-03-01 03:46:35 +0700

    fix(test): update mockery config and generate missing mocks

--- internals/usecase/test/usecase.go
+++ internals/usecase/test/usecase.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gndw/starting-golang/internals/models"
 )
 
-//go:generate mockery --name Usecase
 type Usecase interface {
 	Test(ctx context.Context, request models.TestRequest) (response models.TestResponse, err error)
 }
